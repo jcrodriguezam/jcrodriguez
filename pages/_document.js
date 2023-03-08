5d19015a667d05f573a7d4dc55ea8918bb6338ff
Juan Carlos <jcrodriguezam@outlook.es>
2023-03-08 01:59:53 +0100

    Initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
